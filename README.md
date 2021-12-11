2dc2f439b1ac516bcc61f91e3e77ef46879737fe
Jason Chu <jason@cockroachlabs.com>
2021-12-10 17:09:47 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# log4shelldetect
+
+Scans a file or folder recursively for jar files that may be vulnerable to Log4Shell (CVE-2021-44228) by inspecting the class paths inside the jar.
+
+## License
+
+[Unlicensed](/LICENSE)
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"archive/zip"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/fatih/color"
+	"github.com/karrick/godirwalk"
+)
+
+var printMutex = new(sync.Mutex)
+
+var mode = flag.String("mode", "report", "the output mode, either \"report\" (every jar pretty printed) or \"list\" (list of potentially vulnerable files)")
+
+func main() {
+	flag.Parse()
+
+	if flag.Arg(0) == "" {
+		fmt.Println("Usage: log4shelldetect [options] <path>")
+		fmt.Println("Scans a file or folder recursively for jar files that may be")
+		fmt.Println("vulnerable to Log4Shell (CVE-2021-44228) by inspecting")
+		fmt.Println("the class paths inside the Jar")
+		fmt.Println("")
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	target := flag.Arg(0)
+
+	f, err := os.Stat(target)
+	if err != nil {
+		panic(err)
+	}
+
+	if !f.IsDir() {
+		checkJar(target)
+		return
+	}
+
+	pool := make(chan struct{}, 8)
+
+	err = godirwalk.Walk(target, &godirwalk.Options{
+		Callback: func(osPathname string, de *godirwalk.Dirent) error {
+			if filepath.Ext(osPathname) == ".jar" {
+				pool <- struct{}{}
+				go func() {
+					checkJar(osPathname)
+					<-pool
+				}()
+			}
+
+			return nil
+		},
+		ErrorCallback: func(osPathname string, err error) godirwalk.ErrorAction {
+			log.Printf("skipping %q: %v", osPathname, err)
+			return godirwalk.SkipNode
+		},
+		Unsorted: true, // (optional) set true for faster yet non-deterministic enumeration (see godoc)
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < cap(pool); i++ {
+		pool <- struct{}{}
+	}
+}
+
+func checkJar(pathToFile string) {
+	err := func() error {
+		rd, err := zip.OpenReader(pathToFile)
+		if err != nil {
+			return err
+		}
+
+		var vulnClassFound = false
+		var patchedClassFound = false
+
+		for _, file := range rd.File {
+			if strings.HasSuffix(file.Name, "apache/logging/log4j/core/lookup/JndiLookup.class") {
+				vulnClassFound = true
+			}
+
+			if strings.HasSuffix(file.Name, "apache/logging/log4j/core/lookup/JndiRestrictedLookup.class") {
+				patchedClassFound = true
+			}
+		}
+
+		if !vulnClassFound {
+			printStatus(pathToFile, StatusOK, "")
+		} else if patchedClassFound {
+			printStatus(pathToFile, StatusPatched, "")
+		} else {
+			printStatus(pathToFile, StatusVulnerable, "")
+		}
+
+		return nil
+	}()
+	if err != nil {
+		printStatus(pathToFile, StatusUnknown, "")
+		return
+	}
+}
+
+type Status int
+
+const (
+	StatusOK = iota
+	StatusVulnerable
+	StatusPatched
+	StatusUnknown
+)
+
+func printStatus(fileName string, status int, desc string) {
+	printMutex.Lock()
+	defer printMutex.Unlock()
+
+	if *mode == "list" {
+		if status == StatusVulnerable {
+			fmt.Println(fileName)
+		}
+
+		return
+	}
+
+	var c *color.Color
+	switch status {
+	case StatusOK:
+		c = color.New(color.FgGreen)
+		c.Print("OK      ")
+	case StatusPatched:
+		c = color.New(color.FgGreen)
+		c.Print("PATCHED ")
+	case StatusVulnerable:
+		c = color.New(color.FgRed)
+		c.Print("VULNRBL ")
+	case StatusUnknown:
+		c = color.New(color.FgYellow)
+		c.Print("UNKNOWN ")
+	}
+
+	fmt.Print(fileName)
+
+	if desc != "" {
+		fmt.Print(": " + desc)
+	}
+
+	fmt.Println("")
+}
